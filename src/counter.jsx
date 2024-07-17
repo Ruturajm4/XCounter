4a455be219a2916267948f1c87b2f32fc01b65cc
Ruturajm4 <ruturaj.m4@gmail.com>
2024-07-17 10:09:35 +0530

    commit

---
+++ src/counter.jsx
@@ -0,0 +1,26 @@
+import React, {useState} from "react";
+
+const Xcounter = ()=>{
+    const [count, setCount]=useState(0)
+
+    const handleIncrement= ()=>{
+        setCount((prev)=>prev+1)
+    }
+
+    const handleDecrement = ()=>{
+        setCount((prev)=>prev-1)
+    }
+
+    return(
+        <div>   
+            <h3>Counter App</h3>
+
+            <p>{count}</p>
+
+            <button onClick={handleIncrement}>Increment</button>
+            <button onClick={handleDecrement}>Decrement</button>
+        </div>
+    )
+}
+
+export default Xcounter
